f14b0454eefb3232435529ea26a5aeb87b2b467f
sonamg3000 <sonam.g3000@gmail.com>
2019-03-24 14:33:38 +0530

    refrence. java file

---
+++ reference.java
@@ -0,0 +1,15 @@
+class Animal 
+{
+    String color;
+	int age;
+	   
+	   public static void main (String args[])
+	   {
+	   Animal bb=new Animal();
+	   bb.color="black";
+	  
+	   bb.age=20;
+	   System.out.println(bb.color+" "+bb.age);
+	   }
+
+}
